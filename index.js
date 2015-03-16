03eebbb204ed2692e979198400a266f738502ae1
Sindre Sorhus <sindresorhus@gmail.com>
2015-03-16 17:18:46 +0700

    add user-agent header to the request

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var got = require('got');
 module.exports = function (token, cb) {
 	var headers = {
 		Accept: 'application/vnd.github.v3+json',
-		Authorization: 'token ' + token
+		Authorization: 'token ' + token,
+		'User-Agent': 'https://github.com/kevva/github-token-user'
 	};
 
 	got('https://api.github.com/user', {headers: headers}, function (err, data) {
